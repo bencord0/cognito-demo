89a40c12db345f712084078e50fcb28061a8f906
Ben Cordero <bencord0@condi.me>
2021-12-30 20:42:46 +0000

    Allow setting CSRF_TRUSTED_DOMAINS from env

--- src/cognitodemo/settings.py
+++ src/cognitodemo/settings.py
@@ -157,3 +157,5 @@ STATIC_ROOT = os.getenv('STATIC_ROOT', 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
